57797bea62699ba415e5f9a248a5d8b7d3e46941
JortzFromSchool <johnnydortiz@gmail.com>
2021-09-02 14:00:25 -0400

    my_transpose refactoring

--- rspec_project/lib/rspec_project.rb
+++ rspec_project/lib/rspec_project.rb
@@ -8,6 +8,27 @@ class Array
     hash.keys
   end
 
+  def two_sum
+    result = []
+    self.each_with_index do |ele1, i1|
+      self.each_with_index do |ele2, i2|
+        result << [i1, i2] if (i2 > i1) && (ele1 + ele2 == 0)
+      end
+    end
+    return result
+  end
 
+  def my_transpose
+    result = Array.new()
 
+    self.each_with_index do |row, i|
+      collector = []
+      row.each_with_index do |ele, j|
+        collector << row[i]
+      end
+      result << collector
+    end
+    
+    result
+  end
 end
--- rspec_project/spec/rspec_project_spec.rb
+++ rspec_project/spec/rspec_project_spec.rb
@@ -11,7 +11,19 @@ describe Array do
     expect(arr.my_uniq).to eq([1, 2, 3])
   end
 
+  it "finds all pair positions whose elements sum to zero" do
+    arr = [-1, 0, 2, -2, 1]
+    expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+  end
 
-
-
+  it "convert between row-oriented and col-oriented representations of a square matrix" do
+    arr = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]]
+    expect(arr.my_transpose).to eq([
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]])
+  end
 end
