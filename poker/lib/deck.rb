01f3aa75226a0731267b259915e5107415055108
Woodstock <victorhe97@gmail.com>
2021-09-02 17:01:53 -0400

    Added draw method to Deck and started Hand class.

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,3 +1,5 @@
+require "card.rb"
+
 class Deck
 
     attr_reader :array
@@ -8,17 +10,26 @@ class Deck
         @array.sort!
     end
 
-    SUITS = [:hearts, :spades, :clubs, :diamonds]
-    
+    SUITS = [:diamonds, :clubs, :hearts, :spades]
+
     def populate_deck
         i = 0
         SUITS.each do |suit|
             (1..13).to_a.each do |value|
                 @array << Card.new(suit, value, i)
-                i += 1 
+                i += 1
             end
         end
     end
+
+    def shuffle
+        @array.shuffle
+    end
+
+    def draw
+        @array.pop
+    end
+
 
 
 end
--- poker/spec/poker_spec.rb
+++ poker/spec/poker_spec.rb
@@ -1,38 +1,64 @@
 require "card.rb"
 require "deck.rb"
-
+require "hand.rb"
 
 describe Card do
-  it "initializes the Card with Suit and Integer value" do
-    card = Card.new(:hearts, 5, 3)
-    expect(card.suit).to eq(:hearts)
-    expect(card.value).to eq(5)
+  describe "#initialize" do
+    it "initializes the Card with Suit and Integer value" do
+      card = Card.new(:hearts, 5, 3)
+      expect(card.suit).to eq(:hearts)
+      expect(card.value).to eq(5)
+    end
   end
 end
 
 describe Deck do
-  it "initializes an array of 52 cards" do
-    deck = Deck.new
+  describe "#initialize" do
+    it "initializes an array of 52 cards" do
+      deck = Deck.new
 
-    expect(deck.array.size).to eq(52)
-    deck.array.each do |card|
-      expect(card.is_a?(Card)).to be true
+      expect(deck.array.size).to eq(52)
+      deck.array.each do |card|
+        expect(card.is_a?(Card)).to be true
+      end
     end
 
+    it "all 52 cards are unique" do
+      # create a hash and key array [suit, value]
+      # check there are 52 keys and that each value is 1
+      deck = Deck.new
+      hash = Hash.new(0)
+      expect(deck.array.length).to eq(52)
+
+      deck.array.each { |card| hash[[card.suit, card.value]] += 1 }
+      expect(hash.keys.length).to eq(52)
+    end
   end
 
-
-  it "shuffles the cards in the array" do
-    deck = Deck.new
-    expect(deck.array).to eq(deck.array.sort)
-    expect(deck.array.sort).to not_eq(deck.shuffle)
+  describe "#shuffle" do
+    it "shuffles the cards in the array" do
+      deck = Deck.new
+      expect(deck.array).to eq(deck.array.sort)
+      expect(deck.array.sort).to_not eq(deck.shuffle)
+    end
   end
 
-  it "all 52 cards are unique" do
-    # create a hash and key array [suit, value]
-    # check there are 52 keys and that each value is 1
-
-
+  describe "#draw" do
+    it "takes a card from top of the deck and returns it" do
+      deck = Deck.new
+      last_card = deck.array.last
+      expect(deck.draw).to eq(last_card)
+    end
   end
-
 end
+
+describe Hand do
+  let(:deck) { double("deck") }
+  subject(:hand) { Hand.new(deck) }
+
+  describe "#initialize" do
+    it "grab 5 instances of card from deck" do
+    end
+  end
+end
+
