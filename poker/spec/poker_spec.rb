fd2d20b8c222830e7658ae69b11d7f1bbcc3d344
Woodstock <victorhe97@gmail.com>
2021-09-02 15:48:10 -0400

    Initialized files for poker project. Wrote tests to initialize Card and Deck classes.

---
+++ poker/spec/poker_spec.rb
@@ -0,0 +1,36 @@
+require "card.rb"
+require "deck.rb"
+
+
+describe Card do
+  it "initializes the Card with Suit and Integer value" do
+    card = Card.new(:hearts, 5)
+    expect(card.suit).to eq(:hearts)
+    expect(card.value).to eq(5)
+  end
+end
+
+describe Deck do
+  it "initializes an array of 52 cards" do
+    deck = Deck.new
+
+    expect(deck.array.size).to eq(52)
+    deck.array.each do |card|
+      expect(card.is_a?(Card))
+    end
+
+  end
+
+
+  it "shuffles the cards in the array" do
+
+  end
+
+  it "all 52 cards are unique" do
+    # create a hash and key array [suit, value]
+    # check there are 52 keys and that each value is 1
+
+
+  end
+
+end
